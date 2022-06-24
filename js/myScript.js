ad59dd48b665fdd8bf9e52fc5f8ea37070b0e9ee
pankkap <pankkap@gmail.com>
2022-06-24 10:25:21 +0530

    Day-9 Code

---
+++ js/myScript.js
@@ -0,0 +1,22 @@
+// JavaScript Code
+// JavaScript Statments
+
+// alert("This is JavaScript");
+// Variables
+// JavaScript is typeless
+// loosely types
+var a = 10;
+a = "Welcome to ";
+
+var b = 20;
+b = " JavaScript";
+
+var c = a + b;
+console.log(c);
+
+document.write(c);
+
+alert("The Result is " + c);
+
+document.getElementById("head1").innerText = c;
+document.getElementById("head1").style.display = "none";
